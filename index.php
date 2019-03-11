754f30022009f31f92502bb9f04e1320980d49b1
Tim <tim@timgreen.ws>
2019-03-11 13:15:29 +1100

    Add files via upload

--- index.php
+++ index.php
@@ -19,10 +19,13 @@
 
   <!-- Favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" href="assets/images/favicon/favicon-32x32.png" sizes="32x32" />
-  <link rel="icon" type="image/png" href="assets/images/favicon/favicon-16x16.png" sizes="16x16" />
-  <link rel="icon" type="image/png" href="assets/images/favicon/favicon.ico" sizes="16x16" />
-  <link rel="mask-icon" href="assets/images/favicon/safari-pinned-tab.svg" color="#363558">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+  <link rel="mask-icon" href="assets/images/favicon/safari-pinned-tab.svg" color="#930f5b">
+  <meta name="apple-mobile-web-app-title" content="Orb">
+  <meta name="application-name" content="Orb">
+  <meta name="msapplication-TileColor" content="#930f5b">
+  <meta name="theme-color" content="#ffffff">
 
   <!-- CSS -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"  type="text/css" media="all">
@@ -102,12 +105,12 @@
       <div class="col-xl-8 col-lg-6">
         <div class="about-wrap">
           <ul>
-            <li><i class="fab fa-bluetooth" aria-hidden="true"></i> Pare your PC wiht Bluetooth</li>
+            <li><i class="fab fa-bluetooth" aria-hidden="true"></i> Pair your computer with Bluetooth</li>
             <li><i class="far fa-location" aria-hidden="true"></i> Search any location</li>
             <li><i class="fal fa-desktop" aria-hidden="true"></i> Lock / Unlock the PC</li>
             <li><i class="far fa-search" aria-hidden="true"></i> Find your PC</li>
             <li><i class="far fa-lock-alt" aria-hidden="true"></i> 100% Data secure / privacy</li>
-            <li><i class="fal fa-monitor-heart-rate" aria-hidden="true"></i> Monitor and analise statistics</li>
+            <li><i class="fal fa-monitor-heart-rate" aria-hidden="true"></i> Monitor and analyse statistics</li>
           </ul>
         </div>
       </div>
@@ -122,21 +125,21 @@
     <div class="row">
       <div class="col-lg-4 col-md-6">
         <div class="feature-item">
-          <div class="orb-icon"><img src="build/assets/images/icon10@3x.png" alt="Easy to Use" width="70"></div>
+          <div class="orb-icon orb-icon-fa"><i class="fal fa-mobile"></i></div>
           <h2>Easy to Use</h2>
           <p>Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Donec quam felis, ultricies nec pellentesque.</p>
         </div>
       </div>
       <div class="col-lg-4 col-md-6">
         <div class="feature-item">
-          <div class="orb-icon"><img src="build/assets/images/icon11@3x.png" alt="Light Weight" width="70"></div>
+          <div class="orb-icon orb-icon-fa"><i class="fal fa-weight"></i></div>
           <h2>Light Weight</h2>
           <p>Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Donec quam felis, ultricies nec pellentesque.</p>
         </div>
       </div>
       <div class="col-lg-4 col-md-6">
         <div class="feature-item">
-          <div class="orb-icon"><img src="build/assets/images/icon12@3x.png" alt="Advanced System" width="70"></div>
+          <div class="orb-icon orb-icon-fa"><i class="fal fa-box-open"></i></div>
           <h2>Advanced System</h2>
           <p>Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Donec quam felis, ultricies nec pellentesque.</p>
         </div>
@@ -208,17 +211,17 @@
             <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
               <li class="nav-item">
                 <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
-                  <span class="orb-icon"><img src="build/assets/images/icon16@3x.png" alt="Responsive" width="40"></span>Responsive
+                  <span class="orb-icon"><i class="fal fa-browser"></i></span>Responsive
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">
-                  <span class="orb-icon"><img src="build/assets/images/icon17@3x.png" alt="Support" width="40"></span>Support
+                  <span class="orb-icon"><i class="fal fa-life-ring"></i></span>Support
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">
-                  <span class="orb-icon"><img src="build/assets/images/icon18@3x.png" alt="Free Updates" width="40"></span>Free Updates
+                  <span class="orb-icon"><i class="fal fa-wrench"></i></span>Free Updates
                 </a>
               </li>
             </ul>
@@ -329,7 +332,9 @@
         </div>
       </div>
       <div class="col-lg-6">
-        <div class="orb-icon"><i class="fal fa-life-ring"></i></div>
+        <div class="orb-icon"><img src="build/assets/images/hugo-customer-support.png" alt="illustration by Ouch.pics"></i>
+          <span class="orb-caption">Illustration by Ouch.pics</span>
+        </div>
       </div>
     </div>
   </div>
@@ -342,7 +347,7 @@
       <div class="col-md-6">
         <div class="orb-image">
           <div class="image-wrap">
-            <img src="build/assets/images/device5.png" alt="App">
+            <img src="build/assets/images/device3.png" alt="App">
           </div>
         </div>
       </div>
@@ -366,11 +371,13 @@
     <div class="subscribe-wrap">
       <h2 class="section-title">Join the 9,000+ customers that are already using our app</h2>
       <p>Subscribe our newsletter to recive the latest news and exclusive offers every week.</p>
-      <form>
+      <form action="https://formsubmit.co/118d05d13d98b8f2b20e21c52b0abd96" method="POST" />
         <div class="form-group">
-          <input type="email" class="form-control" id="email" placeholder="Type your email here....">
+          <input name="email" type="email" class="form-control" id="email" placeholder="Type your email here....">
+          <input type="hidden" name="_subject" value="someone tested the orb landing page demo">
+          <input type="text" name="_honey" style="display:none">
         </div>
-        <input type="submit" class="orb-btn" name="Submit" value="subscribe now!">
+        <input type="submit" class="orb-btn" name="Submit" id="subscribe-icn" value="subscribe now!">
       </form>
     </div>
   </div>
